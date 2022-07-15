9ae0debc1d2b030dce9489cfc4695570c31b68db
manishkhadka13 <mandy.khadka10@gmail.com>
2022-07-15 20:50:19 +0545

    armstrong

---
+++ armstrong_checker.py
@@ -0,0 +1,12 @@
+import os
+number=int(input("Enter a number to check armstrong number: "))
+sum=0
+temp=number
+while temp>0:
+    digit=temp%10
+    sum+=digit**3
+    temp//=10
+if number==sum:
+    print(number, "is an armstrong number")
+else:
+    print(number, "is not an armstrong number")
