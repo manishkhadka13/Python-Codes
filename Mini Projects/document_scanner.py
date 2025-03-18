0c320b4529040c25c808cb4ed5a6210b59d9eb6a
Magneto <mandy.khadka10@gmail.com>
2025-03-18 12:51:42 +0100

    Updated minor changes in the codes

--- Mini Projects/document_scanner.py
+++ Mini Projects/document_scanner.py
@@ -9,3 +9,21 @@ import imutils
 ap=argparse.ArgumentParser()
 ap.add_argument("-i","--image",required=True,help="Path to the image to be scanned")
 args=vars(ap.parse_args())
+
+image=cv2.imread(args["image"])
+ratio=image.shape[0]/500.0
+orig=image.copy()
+image=imutils.resize(image,height=500)
+
+#Convert images into grayscale, blur it and find the edges
+gray=cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
+gray=cv2.GaussianBlur(gray,(5,5),0)
+edged=cv2.Canny(gray,75,200)
+
+print("STEP 1: Edge Detection: ")
+cv2.imshow("Image",image)
+cv2.imshow("Edged",edged)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+
